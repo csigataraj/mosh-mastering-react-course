a19f0d1e9e898f7a08fcff6dc9605b24e016d96b
BUDAPEST\Attila_Batky <Attila_Batky@epam.com>
2019-02-19 13:55:03 +0100

    made Like component stateless functional

--- vidly/src/components/common/like.jsx
+++ vidly/src/components/common/like.jsx
@@ -1,23 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Like extends Component {
-  state = { liked: false };
-
-  handleClick() {
-    const liked = this.state.liked ? false : true;
-    this.setState({ liked });
-  }
-
-  render() {
-    // debugger;
-    return (
-      <i
-        onClick={() => this.handleClick()}
-        className={`fa fa-${this.state.liked ? "heart" : "heart-o"} mr-4`}
-        aria-hidden="true"
-      />
-    );
-  }
-}
-
-export default Like;
+export const Like = ({ onClick, liked }) => (
+  <i
+    onClick={onClick}
+    className={`fa fa-heart${liked ? "" : "-o"} mr-4`}
+    aria-hidden="true"
+    style={{ cursor: "pointer" }}
+  />
+);
--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
-import Like from "./common/like";
+import { Like } from "./common/like";
 
 class Movies extends Component {
   state = {
@@ -9,6 +9,15 @@ class Movies extends Component {
 
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+
     this.setState({ movies });
   };
 
@@ -28,6 +37,7 @@ class Movies extends Component {
                 <th>Genre</th>
                 <th>Stock</th>
                 <th>Rate</th>
+                <th />
               </tr>
             </thead>
             <tbody>
@@ -38,7 +48,10 @@ class Movies extends Component {
                   <td>{movie.numberInStock}</td>
                   <td>{movie.dailyRentalRate}</td>
                   <td>
-                    <Like />
+                    <Like
+                      onClick={() => this.handleLike(movie)}
+                      liked={movie.liked}
+                    />
                   </td>
                   <td>
                     <button
